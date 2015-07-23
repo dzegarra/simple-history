d81b3a41a3ba299fa5bc402bf0f56472f71ad829
Daniel Zegarra <dzegarra@greplab.com>
2015-07-23 18:37:13 -0500

    Le doy compatibilidad con requirejs (amd) y agrego algunos metodos para leer y escribir variables en la URL

--- simple-history.js
+++ simple-history.js
@@ -4,26 +4,72 @@
  * Copyright 2011, Jörn Zaefferer
  * Dual licensed under the MIT or GPL Version 2 licenses.
  */
-(function(window, undefined) {
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            global.simplehistory = factory()
+}(this, function () { 'use strict';
 
-  var initial = location.href;
+    var initial = location.href;
+    var matcher  = function() {
 
-  window.SimpleHistory = {
-    supported: !!(window.history && window.history.pushState),
-    pushState: function(fragment, state) {
+    };
+
+    var supported = !!(window.history && window.history.pushState);
+
+    var pushState = function(fragment, state) {
       state = state || {};
       history.pushState(state, null, fragment);
-      this.notify(state);
-    },
-    replaceState: function(fragment, state) {
+      notify(state);
+    };
+
+    var replaceState = function(fragment, state) {
       state = state || {};
       history.replaceState(state, null, fragment);
-    },
-    notify: function(state) {
-      this.matcher(location.pathname + location.search, state);
-    },
-    start: function(matcher) {
-      this.matcher = matcher;
+    };
+
+    var readQuery = function() {
+        return location.search;
+    };
+
+    var queryToObject = function() {
+        var obj = {}, i, p, parts, query = readQuery();
+        if (query!='') {
+            i = query.indexOf('?');
+            query = (i==-1) ? '' : query.substr(i+1);
+            parts = query.split('&');
+            for (i in parts) {
+                p = parts[i].split('=');
+                obj[p[0]] = p[1] || '';
+            }
+        }
+        return obj;
+    };
+
+    var objectToQuery = function(obj) {
+        return Object.keys(obj).reduce(function(a, k){
+            a.push(k+'='+encodeURIComponent(obj[k]));
+            return a
+        },[]).join('&');
+    };
+
+    var setQueryParam = function(name, value) {
+        var params = queryToObject();
+        params[name] = value;
+        this.pushState('?'+objectToQuery(params), params);
+    };
+
+    var getQueryParam = function(name) {
+        var params = queryToObject()
+        return params[name] ? params[name] : '';
+    };
+
+    var notify = function(state) {
+      matcher(location.pathname + location.search, state);
+    };
+
+    var start = function(matcher) {
+      matcher = matcher;
       window.addEventListener("popstate", function(event) {
         // workaround to always ignore first popstate event (Chrome)
         // a timeout isn't reliable enough
@@ -31,9 +77,21 @@
           initial = null;
           return;
         }
-        SimpleHistory.notify(event.state || {});
+        notify(event.state || {});
       }, false);
     }
-  };
 
-}(window));
+    //Métodos y propiedades públicas
+    return {
+        supported: supported,
+        pushState: pushState,
+        replaceState: replaceState,
+        readQuery: readQuery,
+        queryToObject: queryToObject,
+        objectToQuery: objectToQuery,
+        setQueryParam: setQueryParam,
+        getQueryParam: getQueryParam,
+        start: start
+    };
+
+}));
